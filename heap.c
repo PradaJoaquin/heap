41700382b77d208b7088c8a9a08cdf7aec3f3159
FedericoValsagna <fede@LAPTOP-U7DH5IGE.localdomain>
2020-12-05 22:40:50 -0300

    Primer commit

---
+++ heap.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include "vector.h"
+#include "heap.h"
+#define SIZE_MINIMO 8
+#define FACTOR_REDIMENSION 2
+typedef struct heap{
+    vector_t* arreglo;
+    cmp_func_t comparar;
+}heap_t;
+
+heap_t *heap_crear(cmp_func_t cmp){
+    heap_t* heap = malloc(sizeof(heap_t));
+    if(!heap){
+        return NULL;
+    }
+    heap->arreglo = vector_crear_vacio();
+    if(!heap->arreglo){
+        free(heap);
+        return NULL;
+    }
+    heap->comparar = cmp;
+}
+
+heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
+    heap_t* heap = malloc(sizeof(heap_t));
+    if(!heap){
+        return NULL;
+    }
+    heap->arreglo = vector_crear(arreglo, n);
+    if(!arreglo){
+        free(heap);
+        return NULL;
+    }
+    heap->comparar = cmp;
+    return heap;
+}
+
+void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)){
+    vector_destruir(heap->arreglo, destruir_elemento);
+    free(heap);
+}
+
+size_t heap_cantidad(const heap_t *heap){
+    return vector_cantidad(heap->arreglo);
+}
+
+bool heap_esta_vacio(const heap_t *heap){
+    return vector_cantidad(heap->arreglo) == 0;
+}
+
+/* Se guarda al final del array se hace upheap */
+bool heap_encolar(heap_t *heap, void *elem){
+    vector_guardar(heap->arreglo, elem);
+    size_t pos_hijo = vector_cantidad(heap->arreglo);
+    upheap(heap, (pos_hijo - 1) / 2, pos_hijo);
+}
+
+void *heap_ver_max(const heap_t *heap){
+    if(heap_esta_vacio(heap))
+        return NULL;
+    return vector_obtener(heap->arreglo, 0);
+}
+
+/* Lo primero que hace es swappear con el ultimo elemento ya que el vector elimina el ultimo, se hace downheap de la nueva raiz despues*/
+void *heap_desencolar(heap_t *heap){
+    vector_swap(heap,0, vector_cantidad(heap->arreglo));
+    void* dato = vector_eliminar(heap->arreglo);
+    downheap(heap, 0, 1, 2);
+    return dato;
+}
+
+
+void upheap(heap_t* heap, size_t padre, size_t hijo){
+    if(hijo == 0){
+        return;
+    }
+    if(comparar(vector_obtener(heap->arreglo, padre), vector_obtener(heap->arreglo, hijo)) >= 0){
+        return;
+    }
+    vector_swap(heap->arreglo, padre, hijo);
+    size_t nuevo_padre = (hijo - 1) / 2;
+    upheap(heap->arreglo, nuevo_padre, padre);
+}
+
+void downheap(heap_t* heap, size_t padre, size_t hijo_izq, size_t hijo_der){
+    if(hijo_izq >= vector_cantidad(heap->arreglo)){
+        return;
+    }
+    //Si el hijo derecho no existe en el arreglo toma el valor del hijo izquierdo (Probablemente ahorre casos borde esto)
+    hijo_der = hijo_der >= vector_cantidad(heap->arreglo) ? hijo_izq : hijo_der; 
+    size_t maximo = vector_obtener(heap->arreglo, hijo_izq) > vector_obtener(heap->arreglo, hijo_der) ? hijo_izq : hijo_der;
+    if(comparar(vector_obtener(heap->arreglo, padre), vector_obtener(heap->arreglo, maximo) > 0)){
+        return;
+    }
+    vector_swap(heap->arreglo, padre, maximo);
+    size_t nuevo_hijo_izq = maximo * 2 + 1;
+    size_t nuevo_hijo_der = maximo * 2 + 2;
+    downheap(heap, maximo, nuevo_hijo_izq, nuevo_hijo_der);
+}
---
+++ vector.h
@@ -0,0 +1,36 @@
+#ifndef _VECTOR_H
+#define _VECTOR_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*******************************************************************
+ *                DEFINICION DE LOS TIPOS DE DATOS
+ ******************************************************************/
+
+typedef struct vector vector_t;
+
+
+/*******************************************************************
+ *                    PRIMITIVAS DEL VECTOR
+ ******************************************************************/
+
+
+vector_t *vector_crear();
+
+
+void vector_destruir(vector_t* vector, void (*destruir_elemento)(void *e));
+
+/* Devuelve el elemento en el indice pasado por parametro */
+void* vector_obtener(vector_t* vector, size_t pos);
+
+/* Se guarda al FINAL lugar del vector el elemento pasado por parametro, devuelve true/false si hubo o no errores a la hora de guardar */
+bool vector_guardar(vector_t* vector, void* valor);
+/* Se elimina el ULTIMO elemento del vector */
+void* vector_eliminar(vector_t* vector);
+/* Se swappean los elementos de los indices pasados por parametro */
+void vector_swap(vector_t* vector, size_t index1, size_t index2);
+/* Devuelve la cantidad de elementos que hay en el vector */
+size_t vector_cantidad(vector_t* vector);
+
+#endif
